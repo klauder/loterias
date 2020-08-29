992dbde04f1f5c83753a54c67a74c0a08c804c0e
unknown <klauder>
2020-08-29 20:32:58 -0300

    Aula 12 - Two-way databinding

---
+++ src/app/meu-form/meu-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-form',
+  templateUrl: './meu-form.component.html',
+  styleUrls: ['./meu-form.component.css']
+})
+export class MeuFormComponent implements OnInit {
+
+  nome: string = 'abc';
+
+  pessoa: any  = {
+    nome: 'Klauder Dias',
+    idade: 32,
+    endereco: {
+      rua: 'rua',
+      numero: 1,
+      complemento: '',
+      bairro:'',
+      cidade:'Betim',
+      estado:'MG'
+    }
+  }
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
