5511d20ad4235b05f6fb9e5472dacd67893a0c55
Klauder <klauderdias@gmail.com>
2021-03-19 16:04:37 -0300

    Aula 54 - Escutando mudan parâmetros de roteamento

--- src/app/rotas/course-details/course-details.component.ts
+++ src/app/rotas/course-details/course-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-course-details',
@@ -9,13 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 export class CourseDetailsComponent implements OnInit {
 
   id: string;
+  inscricao: Subscription;
 
   constructor(private route: ActivatedRoute) {
-    console.log(this.route);
-    this.id = this.route.snapshot.params['id'];
+    //console.log(this.route);
+    //this.id = this.route.snapshot.params['id']; //Obtém a foto de quando foi carregado, não detecta mudanças.
    }
 
   ngOnInit(): void {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
   }
 
 }
