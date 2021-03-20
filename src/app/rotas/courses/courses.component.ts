7f64b6a3064ea073b9402db5e9d5399e44eece40
Klauder <klauderdias@gmail.com>
2021-03-20 16:20:05 -0300

    Aula 56 - Definindo/Extraindo parrams url (query)

--- src/app/rotas/courses/courses.component.ts
+++ src/app/rotas/courses/courses.component.ts
@@ -1,5 +1,7 @@
 import { CoursesService } from './courses.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,11 +11,46 @@ import { Component, OnInit } from '@angular/core';
 export class CoursesComponent implements OnInit {
 
   courses: any[];
+  pagina: number;
+  insricao: Subscription;
 
-  constructor(private coursesService: CoursesService) { }
+  constructor(
+    private coursesService: CoursesService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.courses = this.coursesService.getCourses();
+    this.insricao = this.route.queryParams.subscribe(
+      (params: any) =>{
+        this.pagina = params['p'];        
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.insricao.unsubscribe();
+  }
+
+  prevPage(){
+    if (this.pagina < 2){
+      this.pagina = 1;
+    }else{      
+      this.pagina--;
+    }
+    this.navegacaoPagina();    
+  }
+  
+  nextPage(){
+    this.pagina++;
+    this.navegacaoPagina();
+  }
+
+  navegacaoPagina(){
+    this.router.navigate(['/courses'],{
+      queryParams:{ 'p': this.pagina } 
+    });
   }
 
 }
