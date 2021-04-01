2f03f4919e86288526d32cee94d843e8aaead1d1
Klauder <klauderdias@gmail.com>
2021-04-01 16:00:21 -0300

    Aula 75: Inicializando valores com ngModel (forms)

--- src/app/formularios/template-form/template-form.component.ts
+++ src/app/formularios/template-form/template-form.component.ts
@@ -7,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: 'Klauder Dias',
+    email: 'klauder@email.com'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // [(ngModel)] TWO-WAY-DATABIND É para quando necessito atualizar o valor
+  // [ngModel] Property Binding é para quando necessito SOMENTE INICIALIZAR o campo com um valor que NÃO SERÁ ATUALIZADO
   onSubmit(form){
     console.log(form);
+    console.log(this.usuario);    
   }
 }
